d23cc88ccbd826889f825ca79d873293a84e1b43
cnechifor <corina.luchian@yahoo.com>
2016-08-03 11:03:47 +0300

    TAS-210 formatting

--- src/main/java/org/alfresco/tester/exception/DataPreparationException.java
+++ src/main/java/org/alfresco/tester/exception/DataPreparationException.java
@@ -1,9 +1,11 @@
 package org.alfresco.tester.exception;
 
-public class DataPreparationException extends Exception {
-	private static final long serialVersionUID = -8896257049656936132L;
+public class DataPreparationException extends Exception
+{
+    private static final long serialVersionUID = -8896257049656936132L;
 
-	public DataPreparationException(String message) {
-		super(String.format("Data Preparation Exception: %s", message));
-	}
+    public DataPreparationException(String message)
+    {
+        super(String.format("Data Preparation Exception: %s", message));
+    }
 }
