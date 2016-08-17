1e0f1f46061f75338e679c3c7fbbd6c4b163d005
Paul Brodner <paul.brodner@ness.com>
2016-08-17 16:30:50 +0300

    TAS-347 add support for getting resource files

--- src/main/java/org/alfresco/utility/Utility.java
+++ src/main/java/org/alfresco/utility/Utility.java
@@ -1,13 +1,59 @@
 package org.alfresco.utility;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.alfresco.utility.exception.TestConfigurationException;
 import org.alfresco.utility.exception.TestObjectNotDefinedException;
+import org.slf4j.Logger;
 
 public class Utility
 {
+    static Logger LOG = LogFactory.getLogger();
 
     public static void checkObjectIsInitialized(Object model, String message) throws Exception
     {
         if (model == null)
             throw new TestObjectNotDefinedException(message);
+    }
+
+    @SuppressWarnings("unused")
+    public static File getResourceTestDataFile(String fileName) throws Exception
+    {
+        LOG.info("Get resource test/resources/testdata/{}", fileName);
+        File resource = new File(Utility.class.getClassLoader().getResource(fileName).getFile());
+        if (resource == null)
+        {
+            throw new TestConfigurationException(String.format("Your file %s was not found in test/resources/testdata folder", fileName));
+        }
+
+        return resource;
+    }
+
+    /**
+     * @param fileName
+     * @return the content of filename found in test/resources/testdata/<filename>
+     * @throws Exception
+     */
+    public static String getResourceTestDataContent(String fileName) throws Exception
+    {
+        StringBuilder result = new StringBuilder("");
+        File file = getResourceTestDataFile(fileName);
+
+        try (Scanner scanner = new Scanner(file))
+        {
+            while (scanner.hasNextLine())
+            {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+            scanner.close();
+        }
+        catch (IOException e)
+        {
+            throw new TestConfigurationException(String.format("Cannot read from file %s. Error thrown: %s", fileName, e.getMessage()));
+        }
+        return result.toString();
     }
 }
