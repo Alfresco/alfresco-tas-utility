8a7027266fdeda4704d8825c1a201106e77b8e42
Paul Brodner <paul.brodner@ness.com>
2016-09-02 12:47:48 +0300

    TAS-386 fix imports

--- src/main/java/org/alfresco/utility/model/PersonModel.java
+++ src/main/java/org/alfresco/utility/model/PersonModel.java
@@ -1,9 +1,5 @@
 package org.alfresco.utility.model;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
