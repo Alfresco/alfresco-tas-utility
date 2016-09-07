3cee552b9e59724a7bd0b8d5bf4f25a6cd90535b
Paul Brodner <paul.brodner@ness.com>
2016-09-07 10:34:53 +0300

    fix error from checkstyle

--- src/main/java/org/alfresco/utility/testrail/TestCaseUploader.java
+++ src/main/java/org/alfresco/utility/testrail/TestCaseUploader.java
@@ -51,7 +51,7 @@ public class TestCaseUploader
         if (annotation != null)
         {
 
-            SectionUtil sectionUtil = new SectionUtil(annotation.section(), allSections);
+            SectionUtil sectionUtil = new SectionUtil(annotation.section());
 
             for (Section tmpSection : allSections)
             {
