ace496baf7f9933faa53157219f27a6ce00ad746
Paul Brodner <paul.brodner@ness.com>
2016-09-08 15:25:51 +0300

    update sample

--- samples/consuming-tas-utility/src/test/java/org/alfresco/sample/SampleTest.java
+++ samples/consuming-tas-utility/src/test/java/org/alfresco/sample/SampleTest.java
@@ -13,43 +13,46 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 @ContextConfiguration("classpath:alfresco-test-context.xml")
-public class SampleTest extends AbstractTestNGSpringContextTests {
-	@Autowired
-	protected ServerHealth serverHealth;
+public class SampleTest extends AbstractTestNGSpringContextTests
+{
+    @Autowired
+    protected ServerHealth serverHealth;
 
-	@Autowired
-	protected DataUser dataUser;
+    @Autowired
+    protected DataUser dataUser;
 
-	@Autowired
-	protected DataSite dataSite;
+    @Autowired
+    protected DataSite dataSite;
 
-	@Autowired
-	protected DataContent dataContent;
+    @Autowired
+    protected DataContent dataContent;
 
-	protected SiteModel testSite;
-	protected FolderModel folderModel;
+    protected SiteModel testSite;
+    protected FolderModel folderModel;
 
-	@BeforeClass(alwaysRun = true)
-	public void setupCifsTest() throws Exception {
-		serverHealth.assertServerIsOnline();
-		testSite = dataSite.createPublicRandomSite();
-		folderModel = FolderModel.getRandomFolderModel();
-	}
+    @BeforeClass(alwaysRun = true)
+    public void setupCifsTest() throws Exception
+    {
+        serverHealth.assertServerIsOnline();
+        testSite = dataSite.createPublicRandomSite();
+        folderModel = FolderModel.getRandomFolderModel();
+    }
 
-	@Test
-	public void creatingFolderInRepoLocation() throws Exception {
-		/*
-		 * this call will create folder 'myTest' under
-		 * '/Sites/<randomPublicSiteName>/documentLibrary' location using default admin user
-		 * specified in default.properties file
-		 */
-		dataContent.usingSite(testSite).createFolder(folderModel.getName());
+    @Test
+    public void creatingFolderInRepoLocation() throws Exception
+    {
+        /*
+         * this call will create folder 'myTest' under
+         * '/Sites/<randomPublicSiteName>/documentLibrary' location using default admin user
+         * specified in default.properties file
+         */
+        dataContent.usingSite(testSite).createFolder(folderModel.getName());
 
-		/*
-		 * this call will create folder 'myTest2' under '/' root folder using
-		 * default admin user specified in default.properties file
-		 */
-		FolderModel myFolder =dataContent.usingRoot().createFolder("myTest2");
-		dataContent.assertContentExist(myFolder);
-	}
+        /*
+         * this call will create folder 'myTest2' under '/' root folder using
+         * default admin user specified in default.properties file
+         */
+        FolderModel myFolder = dataContent.usingRoot().createFolder("MyTestFolder");
+        dataContent.assertContentExist(myFolder);
+    }
 }
