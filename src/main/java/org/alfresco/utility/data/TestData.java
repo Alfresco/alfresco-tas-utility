a5b4b3c38ab1d2124303b2a895aa46ce2fafad2d
bogdanbocancea <bogdan.bocancea@ness.com>
2016-09-13 16:10:15 +0300

    TAS-451: modify addUserToSite() to use current user + TestData formatting

--- src/main/java/org/alfresco/utility/data/TestData.java
+++ src/main/java/org/alfresco/utility/data/TestData.java
@@ -13,182 +13,207 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.google.common.io.Files;
 
-public abstract class TestData<Data> implements DSL<Data> {
-	static Logger LOG = LogFactory.getLogger();
+public abstract class TestData<Data> implements DSL<Data>
+{
+    static Logger LOG = LogFactory.getLogger();
 
-	/**
-	 * This is the currentUser that we will use for creating specific test data
-	 * If none specified we will use the admin user defined in
-	 * default.properties
-	 */
-	private UserModel currentUser;
+    /**
+     * This is the currentUser that we will use for creating specific test data
+     * If none specified we will use the admin user defined in
+     * default.properties
+     */
+    private UserModel currentUser;
 
-	/**
-	 * The current space (location) used in Test Data This can be a Site
-	 * location a User Home location, a Data Dictionary location
-	 */
-	private String currentSpace = "/";
+    /**
+     * The current space (location) used in Test Data This can be a Site
+     * location a User Home location, a Data Dictionary location
+     */
+    private String currentSpace = "/";
 
-	/**
-	 * The current site used in test
-	 */
-	private String currentSite = "";
+    /**
+     * The current site used in test
+     */
+    private String currentSite = "";
 
-	/**
-	 * The last content used when calling {@link #usingResource(String)} method
-	 */
-	private String lastResource = "";
+    /**
+     * The last content used when calling {@link #usingResource(String)} method
+     */
+    private String lastResource = "";
 
-	@Autowired
-	protected TasProperties tasProperties;
+    @Autowired
+    protected TasProperties tasProperties;
 
-	public static String PASSWORD = "password";
-	public static String EMAIL = "%s@tas-automation.org";
+    public static String PASSWORD = "password";
+    public static String EMAIL = "%s@tas-automation.org";
 
-	/**
-	 * Check if <filename> passed as parameter is a file or not based on
-	 * extension
-	 */
-	public static boolean isAFile(String filename) {
-		return Files.getFileExtension(filename).length() == 3;
-	}
+    /**
+     * Check if <filename> passed as parameter is a file or not based on
+     * extension
+     */
+    public static boolean isAFile(String filename)
+    {
+        return Files.getFileExtension(filename).length() == 3;
+    }
 
-	/**
-	 * Call this method if you want to use another user rather than admin
-	 * defined in {@link TasProperties} properties file
-	 * 
-	 * @param user
-	 * @return
-	 */
-	@Override
-	@SuppressWarnings("unchecked")
-	public Data usingUser(UserModel user) {
-		currentUser = user;
-		return (Data) this;
-	}
+    /**
+     * Call this method if you want to use another user rather than admin
+     * defined in {@link TasProperties} properties file
+     * 
+     * @param user
+     * @return
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public Data usingUser(UserModel user)
+    {
+        currentUser = user;
+        return (Data) this;
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public Data usingAdmin() {
-		currentUser = getAdminUser();
-		return (Data) this;
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public Data usingAdmin()
+    {
+        currentUser = getAdminUser();
+        return (Data) this;
+    }
 
-	public UserModel getAdminUser() {
-		return new UserModel(tasProperties.getAdminUser(), tasProperties.getAdminPassword());
-	}
+    public UserModel getAdminUser()
+    {
+        return new UserModel(tasProperties.getAdminUser(), tasProperties.getAdminPassword());
+    }
 
-	@Override
-	public UserModel getCurrentUser() {
-		if (currentUser == null) {
-			usingAdmin();
-		}
-		return currentUser;
-	}
+    @Override
+    public UserModel getCurrentUser()
+    {
+        if (currentUser == null)
+        {
+            usingAdmin();
+        }
+        return currentUser;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public Data usingRoot() throws Exception {
-		setCurrentSpace("");
-		return (Data) this;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public Data usingRoot() throws Exception
+    {
+        setCurrentSpace("");
+        return (Data) this;
+    }
 
-	@Override
-	public String getCurrentSpace() {
-		return currentSpace;
-	}
+    @Override
+    public String getCurrentSpace()
+    {
+        return currentSpace;
+    }
 
-	@Override
-	public void setCurrentSpace(String currentRepositorySpace) {
-		this.currentSpace = currentRepositorySpace;
-	}
+    @Override
+    public void setCurrentSpace(String currentRepositorySpace)
+    {
+        this.currentSpace = currentRepositorySpace;
+    }
 
-	/**
-	 * Resource can be a site path, a folder, file where we want to create new
-	 * content or make assertion. The resource is the current object tested in
-	 * your test
-	 */
-	@Override
-	@SuppressWarnings("unchecked")
-	public Data usingResource(String resource) throws Exception {
-		setLastResource(resource);
-		return (Data) this;
-	}
+    /**
+     * Resource can be a site path, a folder, file where we want to create new
+     * content or make assertion. The resource is the current object tested in
+     * your test
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public Data usingResource(String resource) throws Exception
+    {
+        setLastResource(resource);
+        return (Data) this;
+    }
 
-	/**
-	 * Resource can be a folder, file where we want to create new content or
-	 * make assertion. The resource is the current object tested in your test
-	 */
-	@Override
-	public Data usingResource(ContentModel model) throws Exception {
-		return usingResource(model.getLocation());
-	}
+    /**
+     * Resource can be a folder, file where we want to create new content or
+     * make assertion. The resource is the current object tested in your test
+     */
+    @Override
+    public Data usingResource(ContentModel model) throws Exception
+    {
+        return usingResource(model.getLocation());
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public Data usingSite(String siteId) throws Exception {
-		setCurrentSpace(String.format(getSitesPath(), siteId));
-		setCurrentSite(siteId);
-		return (Data) this;
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public Data usingSite(String siteId) throws Exception
+    {
+        setCurrentSpace(String.format(getSitesPath(), siteId));
+        setCurrentSite(siteId);
+        return (Data) this;
+    }
 
-	@Override
-	public Data usingSite(SiteModel siteModel) throws Exception {
-		Utility.checkObjectIsInitialized(siteModel, "siteModel");
-		return usingSite(siteModel.getId());
-	}
+    @Override
+    public Data usingSite(SiteModel siteModel) throws Exception
+    {
+        Utility.checkObjectIsInitialized(siteModel, "siteModel");
+        return usingSite(siteModel.getId());
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public Data usingUserHome(String username) throws Exception {
-		setCurrentSpace(String.format(getUserHomesPath(), username));
-		return (Data) this;
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public Data usingUserHome(String username) throws Exception
+    {
+        setCurrentSpace(String.format(getUserHomesPath(), username));
+        return (Data) this;
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public Data usingUserHome() throws Exception {
-		setCurrentSpace(String.format(getUserHomesPath(), getCurrentUser().getUsername()));
-		return (Data) this;
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public Data usingUserHome() throws Exception
+    {
+        setCurrentSpace(String.format(getUserHomesPath(), getCurrentUser().getUsername()));
+        return (Data) this;
+    }
 
-	@Override
-	public String getRootPath() throws TestConfigurationException {
-		return "";
-	}
+    @Override
+    public String getRootPath() throws TestConfigurationException
+    {
+        return "";
+    }
 
-	@Override
-	public String getSitesPath() throws TestConfigurationException {
-		return "/Sites/%s/documentLibrary";
-	}
+    @Override
+    public String getSitesPath() throws TestConfigurationException
+    {
+        return "/Sites/%s/documentLibrary";
+    }
 
-	@Override
-	public String getUserHomesPath() throws TestConfigurationException {
-		return "/User Homes/%s";
-	}
+    @Override
+    public String getUserHomesPath() throws TestConfigurationException
+    {
+        return "/User Homes/%s";
+    }
 
-	@Override
-	public String getDataDictionaryPath() throws TestConfigurationException {
-		return "/Data Dictionary";
-	}
+    @Override
+    public String getDataDictionaryPath() throws TestConfigurationException
+    {
+        return "/Data Dictionary";
+    }
 
-	public String getCurrentSite() {
-		return currentSite;
-	}
+    public String getCurrentSite()
+    {
+        return currentSite;
+    }
 
-	public void setCurrentSite(String currentSite) {
-		this.currentSite = currentSite;
-	}
+    public void setCurrentSite(String currentSite)
+    {
+        this.currentSite = currentSite;
+    }
 
-	public String getLastResource() {
-		return lastResource;
-	}
+    public String getLastResource()
+    {
+        return lastResource;
+    }
 
-	public void setLastResource(String lastResource) {
-		this.lastResource = lastResource;
-	}
+    public void setLastResource(String lastResource)
+    {
+        this.lastResource = lastResource;
+    }
 
-	public void setCurrentUser(UserModel testUser) {
-		this.currentUser = testUser;
-	}
+    public void setCurrentUser(UserModel testUser)
+    {
+        this.currentUser = testUser;
+    }
 }
