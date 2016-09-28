f47e34fdda52f78dc319f40484fb4c111acbfa5b
Paul Brodner <paul.brodner@ness.com>
2016-09-28 09:03:40 +0300

    added randomizer to UserModel

--- src/main/java/org/alfresco/utility/model/UserModel.java
+++ src/main/java/org/alfresco/utility/model/UserModel.java
@@ -2,16 +2,20 @@ package org.alfresco.utility.model;
 
 import javax.xml.bind.annotation.XmlAttribute;
 
+import org.alfresco.utility.data.RandomData;
+import org.alfresco.utility.data.TestData;
+
 public class UserModel extends TestModel
 {
     private String username;
     private String password;
     private String domain;
 
-    public UserModel(){
-        
+    public UserModel()
+    {
+
     }
-    
+
     public UserModel(String username, String password)
     {
         this.setUsername(username);
@@ -28,10 +32,15 @@ public class UserModel extends TestModel
         this.password = password;
     }
 
-    @XmlAttribute(name="name")
+    @XmlAttribute(name = "name")
     public String getUsername()
     {
-        return username;
+        if (getDomain() != null)
+            return username;
+        else
+        {
+            return getTenantUsername();
+        }
     }
 
     public void setUsername(String username)
@@ -47,5 +56,23 @@ public class UserModel extends TestModel
     public void setDomain(String domain)
     {
         this.domain = domain;
+    }
+
+    public String getTenantUsername()
+    {
+        return String.format("%s@%s", getUsername(), getDomain());
+    }
+
+    public static UserModel getRandomTenandUser()
+    {
+        UserModel tenant = new UserModel(RandomData.getRandomName("tenant"), TestData.PASSWORD);
+        tenant.setDomain("tenant.com");
+        return tenant;
+    }
+
+    public static UserModel getRandomUserModel()
+    {
+        UserModel tenant = new UserModel(RandomData.getRandomName("user"), TestData.PASSWORD);
+        return tenant;
     }
 }
