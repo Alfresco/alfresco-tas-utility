6775c9878d52a97b6a490084da8e099d770a3a30
Cristina Axinte <cristina.axinte@ness.com>
2016-10-12 16:32:17 +0300

    Added support for Groups and some actions for a group.

---
+++ src/main/java/org/alfresco/utility/data/DataGroup.java
@@ -0,0 +1,74 @@
+package org.alfresco.utility.data;
+
+import static org.alfresco.utility.report.log.Step.STEP;
+
+import org.alfresco.dataprep.GroupService;
+import org.alfresco.utility.model.GroupModel;
+import org.alfresco.utility.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+/**
+ * Data Preparation for Groups
+ * 
+ * @author Cristina Axinte
+ */
+@Service
+@Scope(value = "prototype")
+public class DataGroup extends TestData<DataGroup>
+{
+    @Autowired
+    private GroupService groupService;
+
+    /**
+     * Creates a new random group.
+     * 
+     * @return
+     */
+    public GroupModel createRandomGroup()
+    {
+        String groupName = RandomData.getRandomName("Group");
+        STEP(String.format("DATAPREP: Creating group %s with admin", groupName));
+
+        GroupModel groupModel = new GroupModel(groupName);
+        groupService.createGroup(getAdminUser().getUsername(), getAdminUser().getPassword(), groupModel.getDisplayName());
+
+        return groupModel;
+    }
+
+    /**
+     * Current user is added to the specified group
+     * You can also use the {@link #usingUser(org.alfresco.utility.model.UserModel)}
+     * method for defining a user to be added to the group
+     * 
+     * @return
+     */
+    public GroupModel addUserToGroup(GroupModel groupModel)
+    {
+        STEP(String.format("DATAPREP: Add user %s to group %s", getCurrentUser().getUsername(), groupModel.getDisplayName()));
+
+        groupService.addUserToGroup(getAdminUser().getUsername(), getAdminUser().getPassword(), groupModel.getDisplayName(), 
+                getCurrentUser().getUsername());
+        
+        return groupModel;
+    }
+    
+    /**
+     * Adds list of users to the specified group
+     * 
+     * @param groupModel
+     * @param users
+     * @return
+     */
+    public GroupModel addListOfUsersToGroup(GroupModel groupModel, UserModel...users)
+    {
+        for(UserModel userModel: users)
+        {     
+                usingUser(userModel).addUserToGroup(groupModel);
+        }
+        
+        return groupModel;
+    }
+
+}
