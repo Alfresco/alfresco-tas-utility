066a5a497452f0e4928aef504b017637b2368ffa
Cristina Axinte <cristina.axinte@ness.com>
2017-01-11 11:49:09 +0200

    Added new preference names

--- src/main/java/org/alfresco/utility/constants/PreferenceName.java
+++ src/main/java/org/alfresco/utility/constants/PreferenceName.java
@@ -5,9 +5,11 @@ package org.alfresco.utility.constants;
  */
 public enum PreferenceName
 {
-    SITES_FAVORITES_PREFIX("org.alfresco.share.sites.favourites."),
+    SITES_FAVORITES_PREFIX("org.alfresco.share.sites.favourites.%s"),
     FOLDERS_FAVORITES_PREFIX("org.alfresco.share.folders.favourites"),
-    DOCUMENTS_FAVORITES_PREFIX("org.alfresco.share.documents.favourites");
+    DOCUMENTS_FAVORITES_PREFIX("org.alfresco.share.documents.favourites"),
+    EXT_SITES_FAVORITES_PREFIX("org.alfresco.ext.sites.favourites.%s.createdAt"),
+    EXT_FOLDERS_FAVORITES_PREFIX("org.alfresco.ext.folders.favourites.%s.createdAt");
     
     private final String preferenceName;
 
