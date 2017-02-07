64dc4d638981b1d73946ad37f86aae4762d641de
cagache <claudia.agache@ness.com>
2017-02-07 15:02:10 +0200

    added activity types

--- src/main/java/org/alfresco/utility/constants/ActivityType.java
+++ src/main/java/org/alfresco/utility/constants/ActivityType.java
@@ -4,11 +4,16 @@ public enum ActivityType
 {
     USER_JOINED("org.alfresco.site.user-joined"),
     USER_LEFT("org.alfresco.site.user-left"),
+    USER_ROLE_CHANGED("org.alfresco.site.user-role-changed"),
     FILE_ADDED("org.alfresco.documentlibrary.file-added"),
     FILE_PREVIEWD("org.alfresco.documentlibrary.file-previewed"),
     FILE_DELETED("org.alfresco.documentlibrary.file-deleted"),
     FILE_CREATED("org.alfresco.comments.comment-created"),
-    FOLDER_ADDED("org.alfresco.documentlibrary.folder-added");
+    FILE_LIKED("org.alfresco.documentlibrary.file-liked"),
+    FOLDER_ADDED("org.alfresco.documentlibrary.folder-added"),
+    COMMENT_CREATED("org.alfresco.comments.comment-created"),
+    COMMENT_UPDATED("org.alfresco.comments.comment-updated"),
+    COMMENT_DELETED("org.alfresco.comments.comment-deleted");
 
     private final String activityType;
 
