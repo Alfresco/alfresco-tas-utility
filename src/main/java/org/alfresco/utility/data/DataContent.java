b1914dd51e1be1f22e3594dc7d4664b9730120e2
Alecsandru Prună <alecsandru.pruna@ness.com>
2017-02-08 16:12:41 +0200

    Reduced time from 10 seconds to 1 second for waitUntilContentIsDeleted

--- src/main/java/org/alfresco/utility/data/DataContent.java
+++ src/main/java/org/alfresco/utility/data/DataContent.java
@@ -289,7 +289,7 @@ public class DataContent extends TestData<DataContent>
         String deletedObject = contentService.getNodeRefByPath(getSession(), fullPath);
         while (!StringUtils.isEmpty(deletedObject) && retry < Utility.retryCountSeconds)
         {
-            Utility.waitToLoopTime(10);
+            Utility.waitToLoopTime(1);
             deletedObject = contentService.getNodeRefByPath(getSession(), fullPath);
             retry++;
         }
