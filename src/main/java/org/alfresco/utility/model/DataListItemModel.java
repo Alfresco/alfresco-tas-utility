10d88d08c13a224ae2d40a4b1ed651403165deb0
Paul Brodner <paul.brodner@ness.com>
2017-02-14 11:27:40 +0200

    added doc on DataListItemModel

--- src/main/java/org/alfresco/utility/model/DataListItemModel.java
+++ src/main/java/org/alfresco/utility/model/DataListItemModel.java
@@ -2,19 +2,23 @@ package org.alfresco.utility.model;
 
 import java.util.Map;
 
+/**
+ * {@link https://community.alfresco.com/thread/202546-cmis-data-list}
+ */
 public class DataListItemModel extends ContentModel
 {
     private Map<String, Object> itemProperties;
-    
+
     public DataListItemModel(Map<String, Object> itemProperties)
     {
         setItemProperties(itemProperties);
     }
-    
+
     public Map<String, Object> getItemProperties()
     {
         return itemProperties;
     }
+
     public void setItemProperties(Map<String, Object> itemProperties)
     {
         this.itemProperties = itemProperties;
