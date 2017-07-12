4729cdd9f2e1de362a063f053b14a966a79b6f50
mtifui <mirela.tifui@ness.com>
2017-07-12 16:50:35 +0300

    Added support methods to components versions and automated AONE-18302

--- src/test/java/org/alfresco/utility/application/gui/installer/InstallerEndToEndTests.java
+++ src/test/java/org/alfresco/utility/application/gui/installer/InstallerEndToEndTests.java
@@ -30,16 +30,16 @@ public class InstallerEndToEndTests extends InstallerTest
         STEP ("1. Go to /opt/alfresco-one/java/bin/ and verify JRE version");
         components.assertJREVersionIs("java version \"1.8.0_");
         STEP ("2. Go to /opt/alfresco-one/tomcat/bin/ and verify Tomcat version");
-        //components.assertTomcatVersion("Apache Tomcat/7.0.59");
+        components.assertTomcatVersionIs("Apache Tomcat/7.0.59");
         STEP ("3. Go to tomcat install folder (/opt/alfresco-one/tomcat/lib) and check JDBC");
         components.assertJDBCIs("postgresql-9.4.1211.jre7.jar");
-        STEP ("4. In console go to install folder of postgresql");
+        STEP ("4. In console go to install folder of postgresql and get psql version");
+        components.assertPsqlVersionIs("psql (PostgreSQL) 9.4.4");
         STEP ("5 Verify ImageMagick version.");
+        components.assertImageMagickVersionIs("ImageMagick 7.0.5");
     }
 
-
     @Test(groups={"demo"}, priority=1)
-
     public void uninstallAlfrescoOne() throws Exception {
         STEP("1. Run uninstall from Alfresco installation directory.");
         uninstaller.open();
