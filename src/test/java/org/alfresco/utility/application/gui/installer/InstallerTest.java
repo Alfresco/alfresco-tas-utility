20cf4da5b4ac409b73c6813dce0c9d32d5e3e12c
cagache <claudia.agache@ness.com>
2017-07-12 13:57:35 +0300

    automated AONE-18290:Installation Folder - form

--- src/test/java/org/alfresco/utility/application/gui/installer/InstallerTest.java
+++ src/test/java/org/alfresco/utility/application/gui/installer/InstallerTest.java
@@ -1,8 +1,13 @@
 package org.alfresco.utility.application.gui.installer;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.SystemUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 /**
  * Created by Claudia Agache on 7/10/2017.
@@ -12,4 +17,14 @@ public abstract class InstallerTest extends AbstractTestNGSpringContextTests
 {
     @Autowired
     ACSInstaller installer;
+
+    public File createNotEmptyFolderInOS() throws Exception
+    {
+        File f = (SystemUtils.IS_OS_WINDOWS) ? Paths.get("C:\\tmp\\notEmptyFolder\\subDir").toFile() : Paths.get("/tmp/notEmptyFolder/subDir").toFile();
+        if (!f.exists())
+        {
+            FileUtils.forceMkdir(f);
+        }
+        return f;
+    }
 }
