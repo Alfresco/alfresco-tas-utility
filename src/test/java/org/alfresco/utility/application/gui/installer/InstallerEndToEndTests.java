80d79c28e3c64d8c8ec482c0f81c0ba4bd71d526
cagache <claudia.agache@ness.com>
2017-07-14 17:55:39 +0300

    added a skeleton for AONE-18285

--- src/test/java/org/alfresco/utility/application/gui/installer/InstallerEndToEndTests.java
+++ src/test/java/org/alfresco/utility/application/gui/installer/InstallerEndToEndTests.java
@@ -21,9 +21,89 @@ public class InstallerEndToEndTests extends InstallerTest
         // TODO
     }
 
-    public void installationInAdvancedMode()
+    /**
+     * AONE-18285
+     */
+//    @Test()
+    public void installationInAdvancedMode() throws Exception
     {
-        // TODO
+        STEP("1. Precondition: Alfresco One installer is running - Installation Type form is opened.");
+        navigateToInstallationTypeForm();
+
+        STEP("2. At the 'Installation Type' from select 'Advanced Mode' radio button and click 'Forward'.");
+        installer.onInstallationTypePage().chooseAdvancedInstall();
+        installer.onSetup().clickNext();
+
+        STEP("3. At the 'Select Components' form click 'Forward' button.");
+        installer.onSelectComponentsPage()
+                .assertJavaIsChecked()
+                .assertPostgreSQLIsChecked()
+                .assertLibreOfficeIsChecked()
+                .assertAlfrescoContentServicesIsCheckedAndDisabled()
+                .assertSolr1IsUnchecked()
+                .assertSolr4IsChecked()
+                .assertAlfrescoOfficeServicesIsChecked()
+                .assertWebQuickStartIsUnchecked()
+                .assertGoogleDocsIntegrationIsChecked();
+        installer.onSetup().clickNext();
+
+        STEP("4. At the 'Installation Folder' form leave default value and click 'Forward' button.");
+        installer.onInstallationFolderPage().assertInstallationFolderIs("");
+        installer.onSetup().clickNext();
+
+        STEP("5. At the 'Database Server Parameters' form click 'Forward' button.");
+        installer.onDatabaseParametersPage().assertDatabasePortIs("");
+        installer.onSetup().clickNext();
+
+        STEP("6. At the 'Tomcat Port Configuration' form click 'Forward' button. ");
+        installer.onTomcatPortConfigurationPage();
+        //TODO It contains next fields with default values:
+
+//        Web Server Domain - 127.0.0.1
+//        Tomcat Server Port - 8080
+//        Tomcat Shutdown Port - 8005
+//        Tomcat SSL Port - 8443
+//        Tomcat AJP Port - 8009
+        installer.onSetup().clickNext();
+
+        STEP("7. At the 'LibreOffice Server Port' form click 'Forward' button.");
+        installer.onLibreOfficeServerPortPage();
+        //TODO It contains "LibreOffice Server Port" field with default value 8100.
+
+        installer.onSetup().clickNext();
+
+        STEP("8. At the 'Sharded Solr installation' form click 'Forward' button.");
+        installer.onShardedSolrInstallationPage();
+        //TODO assert No is checked by default.
+        installer.onSetup().clickNext();
+
+        STEP("9. At the 'Alfresco FTP Port' form click 'Forward' button.");
+        installer.onFtpPortPage();
+        //TODO It contains "Port" field with default value 21.
+        installer.onSetup().clickNext();
+
+        STEP("10. At the 'Alfresco RMI Port' form click 'Forward' button.");
+        installer.onRmiPortPage();
+        //TODO It contains "Port" field with default value 50500.
+        installer.onSetup().clickNext();
+
+        STEP("11. At the 'Admin Password' form type password for admin (e.g. admin), confirm it and click 'Forward' button.");
+        installer.onAdminPasswordPage()
+                .setAdminPassword(installer.installerProperties.getAdminPassword())
+                .setRepeatAdminPassword(installer.installerProperties.getAdminPassword());
+        installer.onSetup().clickNext();
+
+        STEP("12. At the 'Install as a service' form click 'Forward' button.");
+        //TODO assert YES is checked by default.
+        installer.onSetup().clickNext();
+
+        STEP("13. At the 'Ready to Install' form click 'Forward' button. ");
+        installer.onSetup().clickNext();
+
+        STEP("14. At the 'Completing the Alfresco One Setup Wizard' form click 'Finish' button.");
+        installer.onCompletingSetupPage().clickFinish();
+        //TODO check Readme file is displayed, Alfresco is started successfully. Alfresco One Share page is displayed.
+
     }
 
     @Test(groups = { "demo" }, priority = 0) public void verifyingComponentsVersion() throws Exception
