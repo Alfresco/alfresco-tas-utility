d7d5fa7cc5547a63ccd910f70cb2ea4fbac7fe6b
Paul Brodner <paul.brodner@ness.com>
2017-07-19 17:17:31 +0300

    server health

--- samples/consuming-tas-utility/src/test/java/org/alfresco/sample/howto/StartupTest.java
+++ samples/consuming-tas-utility/src/test/java/org/alfresco/sample/howto/StartupTest.java
@@ -3,20 +3,31 @@ package org.alfresco.sample.howto;
 import org.alfresco.utility.LogFactory;
 import org.alfresco.utility.data.DataUser;
 import org.alfresco.utility.exception.DataPreparationException;
+import org.alfresco.utility.network.ServerHealth;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 @ContextConfiguration("classpath:alfresco-test-context.xml")
 public class StartupTest extends AbstractTestNGSpringContextTests {
 	Logger LOG = LogFactory.getLogger();
+
 	@Autowired
 	DataUser dataUser;
 
+	@Autowired
+	ServerHealth serverHealth;
+
+	@BeforeClass
+	public void checkAlfrescoServerIsOnline() throws Exception {
+		serverHealth.assertServerIsOnline();
+	}
+
 	@Test
-	public void start() throws DataPreparationException {	
+	public void start() throws DataPreparationException {
 		LOG.info("Creating a random user: {}", dataUser.usingAdmin().createRandomTestUser());
 	}
 }
