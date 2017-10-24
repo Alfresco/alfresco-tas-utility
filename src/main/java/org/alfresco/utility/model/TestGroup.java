b37540a9e9679dde7c5fce4028a0f91bcb69f1f6
Alexandru-Eusebiu Epure <Alexandru.Epure@ness.com>
2017-10-24 11:21:39 +0300

    REPO-2289 : Implement tests in TAS project for 'Groups'    Add testGroup "groups".

--- src/main/java/org/alfresco/utility/model/TestGroup.java
+++ src/main/java/org/alfresco/utility/model/TestGroup.java
@@ -58,6 +58,7 @@ public @interface TestGroup
     public static String SHARE = "share";
     public static String BROWSER = "browser";
     public static String SECURITY = "security";
+    public static String GROUPS = "groups";
 
     public static String WONT_FIX = "wont-fix";
     public static String OS_WIN = "windows"; // this will mark tests that needs to be executed ONLY on Windows
